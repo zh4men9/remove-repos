21ead29db2afd171025a900ca1de9d25ab84f761
zh4men9 <zh4men9@163.com>
2023-02-04 18:50:32 +0800

    fix repos.txt bugs

--- remove-github-repos.sh
+++ remove-github-repos.sh
@@ -1,10 +1,11 @@
-# ⛓项目永久链接：https://github.com/itzhangbao/remove-repos
 # 将 DELETE_KOKEN 和 GithubName 都替换为自己的
 DELETE_KOKEN="33ede4e961e574dfdb204ece0ef20309756cbc9e"
-GithubName="itzhangbao"
+GithubName="zh4men9"
 
 for repName in $(cat repos.txt)
 do
     echo "Delete "$repName
+    # remove CRLF of repName
+    repName=${repName%$'\r'}
     curl -XDELETE -H "Authorization: token ${DELETE_KOKEN}" https://api.github.com/repos/${GithubName}/${repName}
 done
